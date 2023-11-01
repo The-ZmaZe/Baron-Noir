8e7d725dedcb1fdf156c16acb84ef21e2ee61276
The-ZmaZe <thezmaze@protonmail.com>
2023-11-01 14:56:17 +0100

    basic presidential elections

--- main.py
+++ main.py
@@ -31,12 +31,16 @@ class Political_Spectrum:
             self.economical += value
 
     @classmethod
-    def compatibility_relative(cls, s1: cls, s2: cls):
+    def compatibility_relative(cls, s1, s2):
         return (
         abs(s1.social - s2.social),
         abs(s1.institutional - s2.institutional),
         abs(s1.economical - s2.economical),
     )
+
+    @classmethod
+    def compatibility_absolute(cls, s1, s2):
+        return sum(cls.compatibility_relative(s1, s2)) / 3
 
     @classmethod
     def mean(cls, *args):
@@ -45,13 +49,12 @@ class Political_Spectrum:
             social.append(spectrum.social)
             institutional.append(spectrum.institutional)
             economical.append(spectrum.economical)
-        social, institutional, economical = *map(lambda x: sum(x)/len(x), [social, institutional, economical])
-        return cls(social, institutional, economical)
+        ret = tuple(map(lambda x: sum(x)/len(x), [social, institutional, economical]))
+        return cls(ret[0], ret[1], ret[2])
 
 class Country:
-    def __init__(self, name, population):
+    def __init__(self, name):
         self.name = name
-        self.population = population
         self.president = None
 
 class Party:
@@ -80,9 +83,9 @@ class Engine:
         self.calendar = dict()
 
         self.actors = [Actor("Jean-Luc", "Mélenchon", 72), Actor("Emmanuel", "Macron", 45), Actor("Marine", "Le Pen", 55)]
-        self.actors[0].spectrum = Political_Spectrum(0.4, 0.5, 0,4)
+        self.actors[0].spectrum = Political_Spectrum(0.4, 0.5, 0.4)
         self.actors[1].spectrum = Political_Spectrum(0.00, -0.5, -0.7)
-        self.actors[3].spectrum = Political_Spectrum(-0.5, -0.4, -0.7)
+        self.actors[2].spectrum = Political_Spectrum(-0.5, -0.4, -0.7)
 
         self.parties = [Party("LFI", "La France Insoumise"), Party("RE", "Renaissance"), Party("RN", "Rassemblement National")]
         self.countries = [Country("France")]
@@ -96,8 +99,9 @@ class Engine:
         for party in self.parties:
             parties_members[party] = set()
 
-        for actors in self.actors:
-
+        for actor in self.actors:
+            if actor.party:
+                parties_members[actor.party].add(actor)
 
         for party in self.parties:
             if parties_members[party]:
@@ -107,24 +111,22 @@ class Engine:
 
     def presidential_elections(self, candidates):
         print("Élections Présidentielles")
-        electors = 
-        print(electors, "électeurs")
-        abstention = round(0.5 + (1/100) * np.random.standard_normal(), 3)
-        print(f"{abstention * 100}% d'abstention")
-        n_abstention = round(abstention * electors)
-        votes = electors - n_abstention
+        electors = list(actor for actor in self.actors if actor.age >= 18)
+        print(len(electors), "électeurs")
 
         ballot = []
-        for i in range(votes):
-            ballot.append(random.choice(candidates))
+        for actor in self.actors:
+            compatibility = list((candidate, Political_Spectrum.compatibility_absolute(actor.spectrum, candidate.spectrum)) for candidate in candidates)
+            ballot.append(min(compatibility, key=lambda x: x[1])[0])
+
         results = count_votes(ballot)
-        
+
         winner = None
         ordered_results = list(results.items())
         ordered_results.sort(key=lambda x: x[1], reverse=1)
         for i, result in enumerate(ordered_results):
-            print(f"{result[0].fullname()} - {round(result[1] * 100 / votes, 2)}%")
-            if result[1] > (votes // 2):
+            print(f"{result[0].fullname()} - {round(result[1] * 100 / len(self.actors), 2)}%")
+            if result[1] > (len(self.actors) // 2):
                 winner = result[0]
         
         if winner:
